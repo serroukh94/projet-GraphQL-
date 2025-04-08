4e005f15c888979dc806f9016d1eac650b2e830f
Ben Tama Serroukh <88806891+serroukh94@users.noreply.github.com>
2025-04-08 17:09:26 +0200

    feat(pdw-01): Inscription, connexion && jwt done

---
+++ api/src/config.ts
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+
+export const config = {
+  port: process.env.PORT ? parseInt(process.env.PORT, 10) : 4000,
+  jwtSecret: process.env.JWT_SECRET ?? 'default_secret',
+  databaseURL: process.env.DATABASE_URL, 
+};
